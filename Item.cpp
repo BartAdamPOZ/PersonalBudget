a8de1808cd7451558940ac3bd75ee4c577358ddd
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-02-12 17:33:16 +0100

    Add Item class with setters and getters

--- Item.cpp
+++ Item.cpp
@@ -1,1 +1,57 @@
 #include "Item.h"
+
+void Item::setDate(int newDate)
+{
+    if (newDate >= 0)
+        itemDate = newDate;
+}
+
+void Item::setStringDate(string newStringDate)
+{
+    itemDate = newStringDate;
+}
+
+void Item::setUserId(int newUserId)
+{
+    userId = newUserId;
+}
+
+void Item::setItemId(int newItemId)
+{
+    itemId = newItemId;
+}
+
+void Item::setItemName(string newItemName)
+{
+    itemName = newItemName;
+}
+
+void Item::setItemAmount(float newItemAmount)
+{
+    itemAmount = newItemAmount;
+}
+
+int Item::getItemId()
+{
+    return itemId;
+}
+
+int Item::getUserId()
+{
+    return userId;
+}
+
+string Item::getItemName()
+{
+    return itemName;
+}
+
+float Item::getItemAmount()
+{
+    return itemAmount;
+}
+
+int Item::getIntDate()
+{
+    return intDate;
+}
--- Item.h
+++ Item.h
@@ -1,18 +1,40 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <iostream>
 
 using namespace std;
 
 class Item
 {
+    int itemId, userId, intDate;
+    string itemDate, itemName;
+    float itemAmount;
+
+public:
+    Item(int itemId = 0, int userId = 0, string itemDate = "", string itemName = "", float itemAmount = 0.0)
+    {
+        this->itemId = itemId;
+        this->userId = userId;
+        this->itemDate = itemDate;
+        this->itemName = itemName;
+        this->itemAmount = itemAmount;
+    };
+
+    void setDate(int newDate);
+    void setStringDate(string newStringDate);
+    void setUserId(int newUserId);
+    void setItemId(int newItemId);
+    void setItemName(string newItemName);
+    void setItemAmount(float newItemAmount);
+
+    int getItemId();
+    int getUserId();
+    string getItemName();
+    float getItemAmount();
+    int getIntDate();
+
 
 };
-
-
-
-
-
-
 
 #endif
